3a82d03bfd8516ab153e6ce0213e70de6a4ee327
Leonardo Libanori <oleonardo.libanori@gmail.com>
2026-01-30 10:21:34 -0300

    Desafio Concluído!!!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Smartphone Nokia");
+        Nokia nokia = new Nokia("11111", "Nokia G21", "123456", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+        Console.WriteLine("Smartphone iPhone");
+        Iphone iphone = new Iphone("22222", "iPhone 14", "654321", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
